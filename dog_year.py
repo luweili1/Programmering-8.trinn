deacb56a1a2ac5e0b5de7ddb281f6a07280ef80d
Luwei Li <luweil@nrg.no>
2025-12-05 11:05:51 +0100

    Katteår og hundeår

---
+++ dog_year.py
@@ -0,0 +1,13 @@
+menneskeår = int(input("Hvor gammel er hunden i menneskår? "))
+
+if menneskeår == 1:
+    hundeår = 15
+
+elif menneskeår == 2:
+    hundeår = 15 + 9
+
+else:
+    hundeår = 24 + (menneskeår-2)*5
+
+
+print ("En hund som har", menneskeår, "er:", hundeår, "år gammel i hundeår")
---
+++ gruppegenerator.py
@@ -0,0 +1,26 @@
+import random
+
+elever = [
+    "Lawin", "Anders", "Leona", "Tiantian", "Henrik", 
+    "Stevan", "Aarash", "Victoria", "Magnus", "Kristi", 
+    "Sharanya", "Vegar", "Manha", "Nora", "Katharina", 
+    "Benjamin", "Nima", "Sebastian", "Sigrid", "Minh", 
+    "Fedor", "Theodor", "Zitong", "Erik", "Advika", "Marius", 
+    "David", "Oscar"
+]
+
+random.shuffle(elever)
+grupper = [elever[i:i+3] for i in range(0, len(elever), 3)]
+
+
+def print_grupper(grupper):
+    for idx, gruppe in enumerate(grupper, 1):
+        print(f"Gruppe {idx}:")
+        for navn in gruppe:
+            print(f"+{'-'*(len(navn)+2)}+")
+            print(f"| {navn} |")
+            print(f"+{'-'*(len(navn)+2)}+")
+        print("\n")
+
+print_grupper(grupper)
+
